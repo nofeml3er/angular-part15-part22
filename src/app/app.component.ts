e575f19cf94c38081f3493805bc2ccec9af75732
nofeml3er <nattapong.meth@gmail.com>
2021-04-04 18:41:28 +0700

    ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit} from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-part15-part22';
+
+  @ViewChild('productList', {static : true}) //ViewChild
+  productList: ProductListComponent; //ViewChild
+  
+  ngOnInit() : void {
+    this.productList.products = [
+      {
+        name: "samsung",
+        price: 455
+      },
+      {
+        name: "huawei",
+        price: 564
+      },
+      {
+        name: "vivo",
+        price: 523
+      }
+    ]; //ViewChild
+    
+  }
+  constructor(){
+
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductItemComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
